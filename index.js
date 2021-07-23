936bbea79521b4dc4bd9dcacd8549403dc8cc50a
kudoyuto <yuto.kudo@oocl.com>
2021-07-23 16:36:53 +0800

    String Answers

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 // Complete the following procedure, the 'name' variable in all letters to uppercase, output: ' 'HELLO' '.
 var name = 'hello';
-// TODO write your code here...
+console.log(name.toUpperCase());
 
 
 // Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
 var sentence = 'good afternoon, mr mike.';
-//TODO write your code here...
+var upper = sentence.charAt(0).toUpperCase() + sentence.slice(1,4);
+var secUpper = sentence.charAt(5).toUpperCase() + sentence.slice(6,14);
+var thirdUpper = sentence.charAt(16).toUpperCase() + sentence.slice(17,18);
+var fourthUpper = sentence.charAt(19).toUpperCase() + sentence.slice(20,24);
+ 
+console.log(upper +" "+secUpper +" "+ thirdUpper+" "+ fourthUpper);
+
 
 
 // Complete the following procedure to extract the amount of money variable, output: 20.
 var money = '￥20';
+console.log(money.slice(1));
+
 //TODO write your code here...
